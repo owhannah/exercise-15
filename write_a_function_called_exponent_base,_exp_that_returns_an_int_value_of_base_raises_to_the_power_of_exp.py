488c1c2cd1ab7e1e8fd72564dff7ae324ebdea80
Your Name <you@example.com>
2024-01-25 00:17:09 +0800

    write_a_function_called_exponent_base,_exp_that_returns_an_int_value_of_base_raises_to_the_power_of_exp

--- write_a_function_called_exponent_base,_exp_that_returns_an_int_value_of_base_raises_to_the_power_of_exp.py
+++ write_a_function_called_exponent_base,_exp_that_returns_an_int_value_of_base_raises_to_the_power_of_exp.py
@@ -1,1 +1,18 @@
 # Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp
+
+def exponent(base, exp):
+    result = 1
+
+    for _ in range(exp):
+        result *= base
+
+    return result
+
+# Display the case 1 and case 2 example:
+case_1, exp_1 = 2, 5
+result_1 = exponent(case_1, exp_1)
+print(f"{case_1} raise to the power of {exp_1}: {result_1}")
+
+case_2, exp_2 = 5, 4
+result_2 = exponent(case_2, exp_2)
+print(f"{case_2} raise to the power of {exp_2}: {result_2}")
